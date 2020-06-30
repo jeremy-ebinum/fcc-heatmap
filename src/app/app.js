73bbb5229cc593523a9b5e70c598affac507acb6
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-06-30 01:56:14 +0100

    feat: add legend

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,29 @@
 import { select } from "d3-selection";
 import { json } from "d3-fetch";
-import { min, max } from "d3-array";
-import { scaleBand, scaleSequential } from "d3-scale";
-import { interpolateRdYlBu } from "d3-scale-chromatic";
+import { min as d3Min, max as d3Max } from "d3-array";
+import {
+  scaleBand,
+  scaleSequential,
+  scaleLinear,
+  scaleThreshold,
+} from "d3-scale";
+import { interpolateRdYlBu, schemeRdYlBu } from "d3-scale-chromatic";
 import { axisBottom, axisLeft } from "d3-axis";
 import { timeFormat } from "d3-time-format";
 import localApiData from "../data/data";
 
 import "./app.scss";
 
-const margins = { top: 32, right: 48, bottom: 128, left: 160 };
+const margins = { top: 32, right: 48, bottom: 160, left: 160 };
 const svgW = 1392 - margins.left - margins.right;
 const svgH = 664 - margins.top - margins.bottom;
 const dataUrl =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 let dataset;
+let svgElem;
 let baseTemp;
+let minTemp;
+let maxTemp;
 
 const fetchData = async () => {
   let res = null;
@@ -60,10 +68,73 @@ const handleMouseLeave = function () {
   select(this).style("stroke", "none").style("opacity", 0.8);
 };
 
+const createLegend = () => {
+  const legendColors = schemeRdYlBu[11].reverse();
+  const legendWidth = 400;
+  const legendHeight = 250 / legendColors.length;
+
+  const legendThreshold = scaleThreshold()
+    .domain(
+      ((min, max, count) => {
+        const step = (max - min) / count;
+        const array = [...new Array(count - 1)].map((item, i) => {
+          return (min + i + 1) * step;
+        });
+        return array;
+      })(minTemp, maxTemp, legendColors.length)
+    )
+    .range(legendColors);
+
+  const legendX = scaleLinear()
+    .domain([minTemp, maxTemp])
+    .range([0, legendWidth]);
+
+  const legendXAxis = axisBottom(legendX).tickValues(legendThreshold.domain());
+
+  const legend = svgElem
+    .append("g")
+    .attr("id", "legend")
+    .attr("transform", `translate(0, ${svgH + 64})`);
+
+  legend
+    .append("g")
+    .selectAll("rect")
+    .data(
+      legendThreshold.range().map((color) => {
+        const d = legendThreshold.invertExtent(color);
+        // eslint-disable-next-line prefer-destructuring
+        if (d[0] == null) d[0] = legendX.domain()[0];
+        // eslint-disable-next-line prefer-destructuring
+        if (d[1] == null) d[1] = legendX.domain()[1];
+        return d;
+      })
+    )
+    .enter()
+    .append("rect")
+    .style("fill", (d) => legendThreshold(d[0]))
+    .attr("x", (d) => legendX(d[0]))
+    .attr("y", 0)
+    .attr("width", (d) => legendX(d[1]) - legendX(d[0]))
+    .attr("height", legendHeight);
+
+  legend
+    .append("g")
+    .attr("transform", `translate(0, ${legendHeight})`)
+    .call(legendXAxis);
+
+  legend
+    .append("text")
+    .attr("x", legendWidth / 2)
+    .attr("y", legendHeight + 44)
+    .attr("class", "legend-label")
+    .style("text-anchor", "middle")
+    .text("Temperature Range in Celcius");
+};
+
 const runApp = async () => {
   select(".js-d3").html("");
 
-  const svgElem = select(".js-d3")
+  svgElem = select(".js-d3")
     .append("svg")
     .attr("width", svgW + margins.left + margins.right)
     .attr("height", svgH + margins.top + margins.bottom)
@@ -72,6 +143,7 @@ const runApp = async () => {
 
   createTooltip();
 
+  // fetch data from api or fallback to hardcoded data
   let data = await fetchData();
   if (!data) data = localApiData;
 
@@ -81,12 +153,13 @@ const runApp = async () => {
   const years = dataset.map((d) => d.year);
   const months = dataset.map((d) => d.month);
   const tempVariances = dataset.map((d) => d.variance);
-  const minTemp = baseTemp + min(tempVariances);
-  const maxTemp = baseTemp + max(tempVariances);
+  minTemp = baseTemp + d3Min(tempVariances);
+  maxTemp = baseTemp + d3Max(tempVariances);
 
-  const colorScale = scaleSequential()
-    .interpolator(interpolateRdYlBu)
-    .domain([maxTemp, minTemp]);
+  const colorScale = scaleSequential(interpolateRdYlBu).domain([
+    maxTemp,
+    minTemp,
+  ]);
 
   const xScale = scaleBand().domain(years).range([0, svgW]).padding(0.05);
   const xAxis = axisBottom(xScale).tickValues(
@@ -130,6 +203,7 @@ const runApp = async () => {
     .attr("class", "y-label")
     .text("Months");
 
+  // draw heatmap
   svgElem
     .selectAll("rect")
     .data(dataset)
@@ -147,6 +221,8 @@ const runApp = async () => {
     .on("mouseover", handleMouseOver)
     .on("mousemove", handleMouseOver)
     .on("mouseleave", handleMouseLeave);
+
+  createLegend();
 };
 
 export default runApp;
